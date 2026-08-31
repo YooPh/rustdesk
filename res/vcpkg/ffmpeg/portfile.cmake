488d2ff4965edf6446e0994a4b545b10eaf884a9
温耀锋 <395897292@qq.com>
2026-08-31 18:15:21 +0800

    fix: 禁用 Windows QSV 依赖
    移除 Windows 构建中的 mfx-dispatch 和 FFmpeg QSV 选项，避免 POSIX 头文件导致 vcpkg 失败。

--- res/vcpkg/ffmpeg/portfile.cmake
+++ res/vcpkg/ffmpeg/portfile.cmake
@@ -141,9 +141,6 @@ elseif(VCPKG_TARGET_IS_WINDOWS)
 --enable-encoder=hevc_amf \
 --enable-encoder=h264_nvenc \
 --enable-encoder=hevc_nvenc \
---enable-libmfx \
---enable-encoder=h264_qsv \
---enable-encoder=hevc_qsv \
 ")
 
     if(VCPKG_TARGET_ARCHITECTURE STREQUAL "x86")
@@ -207,11 +204,6 @@ endif()
 
 string(APPEND VCPKG_COMBINED_C_FLAGS_DEBUG " -I \"${CURRENT_INSTALLED_DIR}/include\"")
 string(APPEND VCPKG_COMBINED_C_FLAGS_RELEASE " -I \"${CURRENT_INSTALLED_DIR}/include\"")
-
-if(VCPKG_TARGET_IS_WINDOWS)
-    string(APPEND VCPKG_COMBINED_C_FLAGS_DEBUG " -I \"${CURRENT_INSTALLED_DIR}/include/mfx\"")
-    string(APPEND VCPKG_COMBINED_C_FLAGS_RELEASE " -I \"${CURRENT_INSTALLED_DIR}/include/mfx\"")
-endif()
 
 # # Setup vcpkg toolchain
 set(prog_env "")
